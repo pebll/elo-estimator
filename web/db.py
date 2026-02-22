9ef86874dab3391ffc54f283bb1c3873f5ad59c3
lo <leo@brucker.fr>
2026-02-22 23:11:05 +0100

    update database path

--- web/db.py
+++ web/db.py
@@ -6,7 +6,7 @@ import sqlite3
 import os
 from datetime import datetime
 
-DB_PATH = os.path.join(os.path.dirname(__file__), "elo_estimator.db")
+DB_PATH = os.path.join(os.path.dirname(__file__), "data", "elo_estimator.db")
 
 
 def get_connection():
